e964b07b07f9a5eaf97974e8c3e9e2dc27807ba7
YOU <youwg@suninfo.com>
2017-12-26 18:32:32 +0800

    修改

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 /**
  * Created by YOU on 2017/12/5.
  */
-const fs = require('fs')
-const https = require('https')
-const Koa = require('koa2')
-const app = new Koa()
-const bodyParser = require('koa-bodyparser')
+const fs = require('fs');
+const https = require('https');
+const Koa = require('koa2');
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
 const {
   port,
-} = require('./server/config').server
-const staticFiles = require('./server/middleware/static')
-const redirect = require('./server/middleware/redirect')
-const routes = require('./server/routes')()
-const mongoose = require('./server/db')
+} = require('./server/config').server;
+const staticFiles = require('./server/middleware/static');
+const redirect = require('./server/middleware/redirect');
+const routes = require('./server/routes')();
+const mongoose = require('./server/db');
 const {
   preUri
-} = require('./server/config')
+} = require('./server/config');
 
 /*
  const responseTime = require('koa-response-time')
@@ -30,7 +30,7 @@ const {
 
 app.on('error', (err, ctx) => {
   console.log('server err:', err.message)
-})
+});
 
 // 错误封装
 app.context.success = (data, message) => {
@@ -38,44 +38,44 @@ app.context.success = (data, message) => {
     success: '请求成功' || message,
     data: data
   }
-}
-app.context.error = (data, message) => {
+};
+app.context.failure = (data, message) => {
   return {
     error: '未知错误' || message,
     data: data
   }
-}
+};
 
 // 简单错误处理
 app.use(async (ctx, next) => {
   try {
     await next()
   } catch (e) {
-    ctx.status = e.status || 500
-    ctx.body = e
+    ctx.status = e.status || 500;
+    ctx.body = e;
     ctx.app.emit('error', e, ctx)
   }
-})
+});
 
 // 日志打印
 app.use(async (ctx, next) => {
-  console.log(`Process on ${ctx.request.method}:${ctx.request.url}...`)
+  console.log(`Process on ${ctx.request.method}:${ctx.request.url}...`);
   await next()
-})
-app.use(bodyParser())
+});
+app.use(bodyParser());
 // 静态文件
-app.use(staticFiles('/static', __dirname + '/static'))
+app.use(staticFiles('/static', __dirname + '/static'));
 // 注册路由
-app.use(routes)
+app.use(routes);
 
 
 // 在端口3000监听:
-app.listen(port)
+app.listen(port);
 https.createServer({
   key: fs.readFileSync('./ca/test-key.pem'),
   ca: fs.readFileSync('./ca/test-csr.pem'),
   cert: fs.readFileSync('./ca/test-cert.pem'),
   passphrase: '123456',
-}, app.callback()).listen(3001)
-console.log('http server started at port 3000...')
-console.log('https server started at port 3001...')
+}, app.callback()).listen(3001);
+console.log('http server started at port 3000...');
+console.log('https server started at port 3001...');
--- server/controllers/user/controller.js
+++ server/controllers/user/controller.js
@@ -1,20 +1,20 @@
 /**
  * Created by YOU on 2017/12/20.
  */
-const config = require('../../config')
-const modelDir = config.mongo.modelDir
-const host = config.server.host
-const User = require(modelDir).User
+const config = require('../../config');
+const modelDir = config.mongo.modelDir;
+const host = config.server.host;
+const User = require(modelDir).User;
 const {
   randomCode,
   sendMail,
   encryptPassword,
-} = require('../../utils')
+} = require('../../utils');
 
 // 获取用户摘要(userName)
 exports.getUserInfo = async (ctx, next) => {
-  console.log(ctx.params.name)
-  let user = null
+  console.log(ctx.params.name);
+  let user = null;
   try {
     user = await User.findOne({name: ctx.params.name})
   } catch (e) {
@@ -25,27 +25,27 @@ exports.getUserInfo = async (ctx, next) => {
   } else {
     return ctx.throw('用户不存在')
   }
-}
+};
 // 验证用户名
 exports.isUserNameExist = async (ctx, next) => {
-  let user = null
+  let user = null;
   try {
     user = await User.findOne({name: ctx.params.name})
   } catch (e) {
     ctx.throw(e)
   }
   ctx.response.body = ctx.success(!!user)
-}
+};
 // 验证邮箱地址
 exports.isMailExist = async (ctx, next) => {
-  let user = null
+  let user = null;
   try {
     user = await User.findOne({mail: ctx.params.mail})
   } catch (e) {
     ctx.throw(e)
   }
   ctx.response.body = ctx.success(!!user)
-}
+};
 // 注册
 exports.register = async (ctx, next) => {
   let userData = {
@@ -53,8 +53,8 @@ exports.register = async (ctx, next) => {
     password: ctx.request.body.password,
     activeCode: ctx.request.body.name + randomCode(50).replace(/[\/\\?&#=]/g, ''),
     mail: ctx.request.body.mail,
-  }
-  let user = new User(userData)
+  };
+  let user = new User(userData);
   try {
     await user.save()
   } catch (e) {
@@ -73,7 +73,7 @@ exports.register = async (ctx, next) => {
   //   ctx.throw(e)
   // }
   ctx.response.body = ctx.success('注册成功')
-}
+};
 // 验证邮箱
 exports.activeMail = async (ctx, next) => {
   try {
@@ -81,11 +81,11 @@ exports.activeMail = async (ctx, next) => {
   } catch (e) {
     ctx.throw(e)
   }
-}
+};
 // 获取个人详细信息(token)
 exports.getUserInfoBySelf = async (ctx, next) => {
   ctx.response.body = ctx.state.user
-}
+};
 // 更新用户信息
 exports.updateUserBySelf = async (ctx, next) => {
   const userData = {
@@ -94,53 +94,55 @@ exports.updateUserBySelf = async (ctx, next) => {
     followsShow: ctx.response.body.followsShow,
     fansShow: ctx.response.body.fansShow,
     description: ctx.response.body.description,
-  }
+  };
   ctx.response.body = 'welcome'
-}
+};
 // 关注
 exports.doFollow = async (ctx, next) => {
   // ctx.state.user.follows
   ctx.response.body = 'welcome'
-}
+};
 // 取消关注
 exports.undoFollow = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 收藏
 exports.doCollection = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 取消收藏
 exports.undoCollection = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 修改密码
 exports.changePasswordBySelf = async (ctx, next) => {
   if (encryptPassword(ctx.request.body.oldPassword, ctx.state.user.salt) === ctx.state.user.hashedPassword) {
     // 旧密码验证通过
+  } else {
+    ctx.response.body = ctx.failure('原始密码错误')
   }
-}
+};
 
 // 后台获取用户列表（仅非管理员用户）
 exports.getUserInfoByAdmin = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 exports.getUserListByAdmin = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 添加用户
 exports.addUserByAdmin = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 更新用户信息
 exports.changePasswordByAdmin = async (ctx, next) => {
   ctx.response.body = '接口不可用'
-}
+};
 // 删除用户
 exports.deleteUserByAdmin = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
 // 封禁账号
 exports.banUserByAdmin = async (ctx, next) => {
   ctx.response.body = 'welcome'
-}
+};
